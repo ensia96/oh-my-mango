47b83e343c413affef00cac1121a16c617d47889
춤추는망고 <ensia96@gmail.com>
2026-03-11 15:26:09 +0900

    feat: 오케스트레이션 스킬 추가 (src/skill.ts)

--- src/skill.ts
+++ src/skill.ts
@@ -66,8 +66,46 @@ export namespace Skill {
     readonly name = "before-git";
   }
 
+  export namespace Orchestration {
+    export class Client extends Interface {
+      readonly description = "메인 및 서브 세션들을 참고하여 정보를 확보합니다";
+      readonly guideline = {
+        constraints: [
+          "전체적인 방향성과 의도를 파악하여 가장 적절한 결과를 도출해야 합니다",
+        ],
+        instructions: [
+          "자신이 속한 서브 세션을 기준으로 부모인 메인 세션의 내용부터 파악합니다",
+          "명시된 지시사항보다 사용자의 진짜 의도와 관련된 배경지식을 우선시합니다",
+          "추가 정보가 필요하면 같은 메인 세션에 속한 다른 서브 세션들을 참고합니다",
+        ],
+      };
+      readonly name = "orchestration-client";
+    }
+
+    export class Server extends Interface {
+      readonly description = "메인 세션과 서브 세션들이 연계되도록 조율합니다";
+      readonly exclusive = "mango";
+      readonly guideline = {
+        constraints: [
+          "전문가 작업에는 오케스트레이션 클라이언트 스킬이 반드시 선행되어야 합니다",
+          "세션 간 연계와 요구사항 전달에만 집중하고 직접적으로 개입하지 않습니다",
+          "전달해야 하는 내용을 왜곡하거나 재해석하거나 과도하게 가공하지 않습니다",
+        ],
+        instructions: [
+          "전문가에게 반드시 오케스트레이션 클라이언트 스킬을 사용하도록 지시합니다",
+          "기존에 있던 서브 세션을 적극적으로 재활용하여 연계의 효율을 극대화합니다",
+          "사용자의 메시지는 핵심만 간략하게 제공하여 전문가의 빠른 이해를 돕습니다",
+          "전문가의 답변은 핵심 내용 원문과 요약을 제공하여 전체 효율성을 높입니다",
+        ],
+      };
+      readonly name = "orchestration-server";
+    }
+  }
+
   export const pack: Record<string, Skill.Interface> = [
     new BeforeGit(),
+    new Orchestration.Client(),
+    new Orchestration.Server(),
   ].reduce((pack, skill) => ({ ...pack, [skill.name]: skill }), {});
 
   export function sync() {
